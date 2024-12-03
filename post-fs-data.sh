d4e58636e6b321b13ea66994321a325989993b3a
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-03 11:04:25 +0800

    mount at only two folders deep

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -18,7 +18,7 @@ cd $MODDIR
 # remove symlinks, remove system/product -> ../product shit
 find ./ -maxdepth 2 -type l -delete
 
-for i in $(ls -d */*/*/*); do
+for i in $(ls -d */*); do
 	mkdir -p $basefolder/$modid/$i
 	mount --bind $MODDIR/$i $basefolder/$modid/$i
 	mount -t overlay -o "lowerdir=$basefolder/$modid/$i:/$i" overlay /$i
