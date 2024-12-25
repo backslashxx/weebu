e396d3aa8e48ddef409f408bddccdb410e942805
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-25 15:49:54 +0800

    scripts/post-fs-data: handle conditional KSU_MAGIC_MOUNT
    https://github.com/5ec1cff/KernelSU/pull/5

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -19,14 +19,20 @@ SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
 mkdir $basefolder/$modid
 ${SUSFS_BIN} add_sus_path $basefolder/$modid
 
-cd $MODDIR/system
+# https://github.com/5ec1cff/KernelSU/commit/92d793d0e0e80ed0e87af9e39879d2b70c37c748
+# on overlayfs, moddir/system/product is symlinked to moddir/product
+# on magic, moddir/product it symlinked to moddir/system/product
+BASE_DIR=$MODDIR
+[ "$KSU_MAGIC_MOUNT" = "true" ] && BASE_DIR="$MODDIR/system"
+cd $BASE_DIR
+
 # remove symlinks, remove system/product -> ../product shit
 # this is optional
 # find ./ -maxdepth 2 -type l -delete
 
 for i in $(ls -d */*); do
 	mkdir -p $basefolder/$modid/$i
-	mount --bind $MODDIR/system/$i $basefolder/$modid/$i
+	mount --bind $BASE_DIR/$i $basefolder/$modid/$i
 	mount -t overlay -o "lowerdir=$basefolder/$modid/$i:/$i" overlay /$i
 	${SUSFS_BIN} add_sus_mount /$i
 done
